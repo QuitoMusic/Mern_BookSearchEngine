588414d5bcd1d9243fa33f1a4359234ef3ce82ee
Francisco Gonzalez <f86gonzalez@outlook.com>
2023-09-19 19:34:25 -0400

    Import AuthenticationError from apollo-server-express/Modify the authMiddleware function to accept the req object as its argument/Use throw new AuthenticationError('Invalid token') to handle authentication errors within GraphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,39 +1,30 @@
 const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server-express');
 
-// set token secret and expiration date
+// Set token secret and expiration date
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+  // Middleware for authentication in GraphQL context
+  authMiddleware: function ({ req }) {
+    // Allows token to be sent via headers
+    const token = req.headers.authorization || '';
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return;
     }
 
-    // verify token and get user data out of it
     try {
+      // Verify token and get user data out of it
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      return { user: data };
+    } catch (error) {
+      throw new AuthenticationError('Invalid token');
     }
-
-    // send to next endpoint
-    next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
